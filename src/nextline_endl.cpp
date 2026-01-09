0d9b3e930a339b4b6c09757c24b89c582a54616c
Arbish Randhawah <arbishtehseen20@gmail.com>
2026-01-09 19:28:01 +0500

    Add files via upload

---
+++ src/nextline_endl.cpp
@@ -0,0 +1,12 @@
+// #include<iostream>
+// using namespace std;
+// int main(){
+//	  cout<<"Hello World!"<<endl;
+//	  cout<<"hello world"; 
+// }
+
+#include<iostream>
+using namespace std;
+int main(){
+	cout<<"Hello World!"<<endl<<"hello world!";
+}
